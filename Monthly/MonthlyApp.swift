e5bc0de50a52a2c90a4d7f77ff9abd20f3f2dc70
Carlos Henrique Matos Borges <CARLOS.COMPASSO@fleetcor.com.br>
2023-12-10 18:57:57 -0300

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Carlos Henrique Matos Borges on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
